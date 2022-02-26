2575e04429ecd0489fc7ea4e3208d4a13687b200
CherifiMi <cherifi154949@gmail.com>
2022-02-26 18:41:33 +0100

    Hide SystemBars and set colors

--- app/src/main/java/com/example/midi/MainActivity.kt
+++ app/src/main/java/com/example/midi/MainActivity.kt
@@ -1,38 +1,57 @@
 package com.example.midi
 
 import android.os.Bundle
+import android.view.WindowInsetsController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.example.midi.ui.theme.MidiTheme
+import com.example.midi.ui.theme.StrockColor
+import com.example.midi.ui.theme.YellowBackColor
 
 class MainActivity : ComponentActivity() {
+
+    //------------values
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        hideSystemBars()
+
         setContent {
             MidiTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                Surface(
+                    color = YellowBackColor,
+                    modifier = Modifier.fillMaxSize()
+                ){
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    private fun hideSystemBars() {
+        val windowInsetsController =
+            ViewCompat.getWindowInsetsController(window.decorView) ?: return
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MidiTheme {
-        Greeting("Android")
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
     }
+
 }
+
--- app/src/main/java/com/example/midi/ui/theme/Color.kt
+++ app/src/main/java/com/example/midi/ui/theme/Color.kt
@@ -6,3 +6,12 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+
+val YellowBackColor = Color(0xFFFFDB47)
+val StrockColor = Color(0xFF0274BD)
+val LightBlueColor = Color(0xFF88EDE7)
+val DarkBlueColor = Color(0xFF20A69E)
+val LightPinkColor = Color(0xFFFFB0CD)
+val DarkPinkColor = Color(0xFFFF7AAB)
+
