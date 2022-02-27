169170bad1c46347d09f1c069340b748bf967b56
CherifiMi <cherifi154949@gmail.com>
2022-02-27 01:56:49 +0100

    drawing the processing Oscilloscope

--- app/src/main/java/com/example/midi/MainActivity.kt
+++ app/src/main/java/com/example/midi/MainActivity.kt
@@ -1,37 +1,38 @@
 package com.example.midi
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.WindowInsetsController
-import androidx.activity.ComponentActivity
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
+import androidx.fragment.app.FragmentActivity
 import com.example.midi.ui.components.MainNotes
 import com.example.midi.ui.components.SharpNotes
+import com.example.midi.ui.components.osilloscope.Oscilloscope
 import com.example.midi.ui.theme.*
 import com.example.midi.ui.viewModel.MainViewModel
+import processing.android.CompatUtils
+import processing.android.PFragment
+import processing.core.PApplet
 
 @ExperimentalComposeUiApi
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
 
     //------------values
+    private var sketch: PApplet? = null
     private val mainViewModel: MainViewModel by viewModels()
+    var hi = this
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,12 +52,29 @@ class MainActivity : ComponentActivity() {
                         .weight(4f)
                         .fillMaxSize()
                     ){
+
                         Box(modifier = Modifier
-                            .weight(6f).background(LightPinkColor)
+                            .weight(6f)
+                            .background(LightBlueColor)
                             .fillMaxSize()
-                        )
+                        ){
+                            AndroidView(
+                                factory = { context: Context ->
+                                    val view = LayoutInflater.from(context)
+                                        .inflate(R.layout.text_layout, null, false)
+
+                                    view.id = CompatUtils.getUniqueViewId()
+                                    sketch = Oscilloscope(mainViewModel)
+                                    val fragment = PFragment(sketch)
+                                    fragment.setView(view, this@MainActivity)
+
+                                    view
+                                })
+                        }
+
                         Box(modifier = Modifier
-                            .weight(4f).background(LightBlueColor)
+                            .weight(4f)
+                            .background(LightBlueColor)
                             .fillMaxSize()
                         )
                     }
@@ -84,10 +102,15 @@ class MainActivity : ComponentActivity() {
         windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        sketch?.onRequestPermissionsResult(
+            requestCode, permissions, grantResults
+        )
+    }
+
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
         setIntent(intent)
     }
-
 }
-
